55db437ae620b34a3b19640e2b56d3cf3b37fbb8
zzezl <maxmandarin29@gmail.com>
2025-08-01 13:37:01 +0300

    2

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const calculateTotal = (price) => {
     const percent = 10
 
     const totalPrice = price
-        .map(value => Number(value.price))
+        .map(value => Number(value.price * value.quantity))
         .reduce((acc, cur) => acc + cur)
 
-    const howMuchInCart = price.map(item => item.price).length
+    const howMuchInCart = price.length
 
     let result = howMuchInCart > 3
         ? totalPrice * (1-percent/100)
@@ -22,7 +22,26 @@ const calculateTotal = (price) => {
     // if(howMuchInCart > 3) {
     //     result = totalPrice*(1-percent/100)
     // }
-    console.log(`До оплати: ${result.toFixed(2)}`)
+    const aboveOne = price.map(item => {
+        if (item.quantity >= 2) {
+            console.log(`${item.name} - ${item.price}грн x${item.quantity} (${item.price * item.quantity}грн)`)
+        }
+    })
+    // console.log(`До оплати: ${result.toFixed(2)}грн`)
+    const sortedByPrice = [...price].sort((a, b) => a.price - b.price)
+    const namePrice = sortedByPrice.map(i => {return `${i.name} - ${i.price}грн x${i.quantity} (${i.price * i.quantity}грн)`})
+
+    console.log
+    (`
+           ЧЕК
+    
+---------------------------
+${namePrice.join('\n')}
+---------------------------
+    
+До оплати ${result.toFixed(2)}грн
+    `)
 }
+
 // const prices = cart.map(item => Number(item.price))
 calculateTotal(cart);
